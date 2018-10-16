d3567c2cdd7dcd3948e92637dff2aca3397072d4
尹冲（虫趣儿） <1606548133@qq.com>
2018-10-16 17:11:04 +0800

    first src

---
+++ src/first.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cfun
+ * Date: 2018/10/16
+ * Time: 16:59
+ */
+class people{
+
+    public function hello()
+    {
+      echo 'hello world!';
+    }
+}
